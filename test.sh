28506389615e0dea2b9b0c7aefd60ef47f4cb3c4
Yusuke Tanaka <yusuktan@maguro.dev>
2020-04-17 00:23:02 +0900

    Implement unary operators: +, -

--- test.sh
+++ test.sh
@@ -25,5 +25,9 @@ assert 47 "5+6*7"
 assert 15 "5*(9-6)"
 assert 4 "(3+5)/2"
 assert 4 "(3+5+1)/2"
+assert 10 "-10+20"
+assert 200 "-10*-20"
+assert 2 "-20/-10"
+assert 5 "+20/+4"
 
 echo OK
--- toycc.c
+++ toycc.c
@@ -139,7 +139,10 @@ Node *new_node_num(int val) {
   return node;
 }
 
-Node *expr(); // prototype
+Node *expr();
+Node *primary();
+Node *mul();
+Node *unary();
 
 Node *primary() {
   if (consume('(')) {
@@ -151,14 +154,22 @@ Node *primary() {
   return new_node_num(expect_number());
 }
 
+Node *unary() {
+  if (consume('+'))
+    return primary();
+  else if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
+}
+
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for(;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
